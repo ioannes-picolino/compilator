2cda4f4fe5f1b08a19800f7a823269bc420a6932
Ivan Luiz <picolotteivan@tutanota.com>
2025-10-17 19:35:14 -0300

    Add files via upload

---
+++ compilator/resources/MainClass.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MainClass{ 
+ public static void main(String args[]){
+   Scanner _key = new Scanner(System.in);
+double  taxa;
+double  capital;
+double  precoVenda;
+double  receita;
+double  receitaTotal;
+double  margemLucro;
+double  precoComMargem;
+double  tempo;
+double  custoFixo;
+double  custoTotal;
+double  custoVariavel;
+double  jurosSimples;
+double  custo;
+double  quantidade;
+double  montante;
+double  lucro;
+ }}
